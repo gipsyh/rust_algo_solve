189e645da6f9414f6ffa72eabc670a7a72c2d8f8
GipsyU <su_yuheng@foxmail.com>
2021-04-30 14:31:07 +0800

    codeforce 1519D.

--- src/main.rs
+++ src/main.rs
@@ -35,31 +35,32 @@ macro_rules! read {
     };
 }
 fn main() {
-    for _ in 0..read!(i64) {
-        let n = read!(usize);
-        let u = read_vec!(usize);
-        let s = read_vec!(i64);
-        let mut idx: Vec<usize> = (0..n).collect();
-        let mut v: Vec<Vec<usize>> = vec![vec![]; n];
-        let mut ans = vec![0; n];
-        idx.sort_by_key(|a| s[*a] * -1);
-        for x in &idx {
-            v[u[*x] - 1].push(*x);
-        }
-        for v in v {
-            let mut tmp = 0;
-            let mut pre: Vec<i64> = vec![];
-            for x in &v {
-                tmp += s[*x];
-                pre.push(tmp);
-            }
-            for i in 0..v.len() {
-                ans[i] += pre[v.len() / (i + 1) * (i + 1) - 1];
-            }
-        }
-        for x in ans {
-            print!("{} ", x);
-        }
-        println!();
+    let n = read!(usize);
+    let a = read_vec!(i64);
+    let b = read_vec!(i64);
+    let mut sum = 0 as i64;
+    for i in 0..n {
+        sum += a[i] * b[i];
     }
+    let mut ans = sum;
+    for i in 0..n {
+        let mut tmp = sum;
+        for j in 1..i.min(n - i - 1) + 1 {
+            tmp = tmp - a[i - j] * b[i - j] - a[i + j] * b[i + j]
+                + a[i - j] * b[i + j]
+                + a[i + j] * b[i - j];
+            ans = ans.max(tmp);
+        }
+        let mut tmp = sum;
+        if i == n - 1 {
+            break;
+        }
+        for j in 0..i.min(n - i - 2) + 1 {
+            tmp = tmp - a[i - j] * b[i - j] - a[i + j + 1] * b[i + j + 1]
+                + a[i - j] * b[i + j + 1]
+                + a[i + j + 1] * b[i - j];
+            ans = ans.max(tmp);
+        }
+    }
+    println!("{}", ans);
 }
