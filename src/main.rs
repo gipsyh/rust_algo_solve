0866ff38cf7704682c26f299115a5ea7281c4eed
GipsyU <su_yuheng@foxmail.com>
2021-05-21 13:36:40 +0800

    codeforce 1527C

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use std::io;
 
 #[allow(dead_code)]
@@ -35,4 +36,18 @@ macro_rules! read {
     };
 }
 
-fn main() {}
+fn main() {
+    for _ in 0..read!(usize) {
+        let n = read!(usize);
+        let v = read_vec!(i64);
+        let mut a: BTreeMap<i64, i64> = BTreeMap::new();
+        let mut ans = 0;
+        let mut last = 0;
+        for i in 0..n {
+            last += *a.entry(v[i]).or_insert(0);
+            ans += last;
+            *a.entry(v[i]).or_insert(0) += (i + 1) as i64;
+        }
+        println!("{}", ans);
+    }
+}
